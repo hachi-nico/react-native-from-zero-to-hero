25f5d69cfc69daa6f724238f445c35f1fbcfcf14
hachi-nico <nicokun48@gmail.com>
2021-09-21 19:49:01 +0700

    done flexbox practice

--- App.js
+++ App.js
@@ -21,12 +21,39 @@ import {
 const App = () => {
   return (
     <SafeAreaView style={styles.body}>
-      <View style={styles}>
-        <Text style={styles.text}>Flex</Text>
-        <Button
-          title="My Github"
-          onPress={() => Linking.openURL('https://github.com/hachi-nico')}
-        />
+      {/* first row */}
+      <View style={styles.row}>
+        <View style={styles.view1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text style={styles.text}>3</Text>
+        </View>
+      </View>
+
+      {/* second row */}
+      <View style={styles.row}>
+        <View style={styles.view4}>
+          <Text style={styles.text}>4</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.view5}>
+          <Text style={styles.text}>5</Text>
+        </View>
+      </View>
+
+      {/* third row */}
+      <View style={styles.big_row}>
+        <View style={styles.view6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+        <View style={styles.view7}>
+          <Text style={styles.text}>7</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -35,15 +62,67 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: 'gray',
-    padding: 10,
+    backgroundColor: '#ffffff',
+  },
+  row: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  big_row: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
     alignItems: 'stretch',
     justifyContent: 'center',
   },
+  view1: {
+    flex: 1,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 2,
+    backgroundColor: '#ff00ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 3,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view4: {
+    flex: 1,
+    backgroundColor: '#ff0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view5: {
+    flex: 1,
+    backgroundColor: '#0fff0f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view6: {
+    flex: 1,
+    backgroundColor: '#eaeaea',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view7: {
+    flex: 1,
+    backgroundColor: '#0000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   text: {
-    color: 'white',
-    fontSize: 20,
-    alignSelf: 'center',
+    color: '#000000',
+    fontSize: 35,
+    margin: 10,
   },
 });
 
