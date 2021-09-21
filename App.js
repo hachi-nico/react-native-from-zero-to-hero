5597a5840f8b6bf4520db2262f5e5dc72c08179c
hachi-nico <nicokun48@gmail.com>
2021-09-21 19:57:34 +0700

    initial commit

--- App.js
+++ App.js
@@ -10,110 +10,13 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.body}>
-      {/* first row */}
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-
-      {/* second row */}
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-
-      {/* third row */}
-      <View style={styles.big_row}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={styles.body}></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: '#ffffff',
-  },
-  row: {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  big_row: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 2,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 3,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: '#0fff0f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: '#eaeaea',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#000000',
-    fontSize: 35,
-    margin: 10,
   },
 });
 
